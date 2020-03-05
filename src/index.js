d4f43c8ff0c9e19311a046849a16e1a2d25c71af
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2020-03-05 15:29:22 +0300

    init: start algorithmic task

--- src/index.js
+++ src/index.js
@@ -35,10 +35,38 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    "**********": ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr2 = [];
+    let arr = expr.split(/(\d{10})/);
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i].length > 9) {
+            arr2.push(arr[i])
+        }
+    }
+    let morse = []
+    for (let i = 0; i < arr2.length; i++){
+        arr2[i]= arr2[i].split(/(\d{2})/)
+        for (let j = 0; j < arr2[i].length; j++){
+            if(arr2[i][j] === "10") {
+                arr2[i][j] = ".";
+            }
+            if(arr2[i][j] === "11") {
+                arr2[i][j] = "-";
+            }
+            if(arr2[i][j] === "00") {
+                arr2[i][j] = "";
+            }
+
+        }
+     morse.push( arr2[i].join(""))
+    }
+    for (let i = 0; i < morse.length; i++){
+        morse[i] = MORSE_TABLE[morse[i]]
+    }
+ return morse.join("")
 }
 
 module.exports = {
